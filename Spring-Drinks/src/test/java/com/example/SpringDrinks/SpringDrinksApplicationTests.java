38d643141e4c83ed1a1b4c7d92425994ed2cbd2e
pddenzil <pddenzil@gmail.com>
2022-01-05 09:40:26 +0000

    initial commit

---
+++ Spring-Drinks/src/test/java/com/example/SpringDrinks/SpringDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
